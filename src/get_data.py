4221861d31b0923396a0b79d93a3cda605503744
Yogesh <yogeshkhagode.99@gmail.com>
2021-09-27 21:40:30 +0530

    first commit

--- src/get_data.py
+++ src/get_data.py
@@ -19,7 +19,7 @@ def get_data(config_path):
     return df
 
 
-
+## Main File function
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
